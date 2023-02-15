6dbf27d16d59483cd69b573696a4cab2148bc4fe
snow-fox <taijunjet@hotmail.com>
2023-02-15 17:38:59 +0000

    add notifications structure

--- src/Farama_Notifications/notices.py
+++ src/Farama_Notifications/notices.py
@@ -1,1 +1,5 @@
-notices = dict()
+# notifications are a dict of dicts
+notifications = dict()
+notifications["gymnasium"] = dict()
+
+notifications["gymnasium"]["69.420"] = "This version does not exist."

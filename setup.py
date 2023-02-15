8c0489efbd645e6a3591d2808822ae0ab960ee6c
snow-fox <taijunjet@hotmail.com>
2023-02-15 17:26:21 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Farama-Notifications",
+    version="0.0.1",
+    author="Jordan Terry",
+    author_email="Jkterry@farama.org",
+    description="Notifications for all Farama Foundation maintained libraries.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Farama-Foundation/Farama-Notifications",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)

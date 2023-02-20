e099ad87d67d1cafcaac454ad3cc6075031a2b7e
snow-fox <taijunjet@hotmail.com>
2023-02-20 15:56:30 +0000

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Farama-Notifications",
-    version="0.0.1",
+    version="0.0.2",
     author="Jordan Terry",
     author_email="Jkterry@farama.org",
     description="Notifications for all Farama Foundation maintained libraries.",

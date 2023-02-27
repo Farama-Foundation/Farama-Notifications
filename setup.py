c2352257d819a0729e0dc68bc0e2bfcc2009f3a0
jjshoots <taijunjet@hotmail.com>
2023-02-27 18:23:11 +0000

    version bnump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Farama-Notifications",
-    version="0.0.3",
+    version="0.0.4",
     author="Jordan Terry",
     author_email="Jkterry@farama.org",
     description="Notifications for all Farama Foundation maintained libraries.",

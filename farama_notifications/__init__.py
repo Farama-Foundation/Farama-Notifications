d98b4e89be3d0075596e99a830c5b20398db4cfe
Kallinteris Andreas <30759571+Kallinteris-Andreas@users.noreply.github.com>
2026-04-22 11:03:28 +0000

    improve message

--- farama_notifications/__init__.py
+++ farama_notifications/__init__.py
@@ -8,12 +8,7 @@ notifications = dict()
 # notifications["project_name"]["version_number"] = "Notification message."
 
 GYMNASIUM_ROBOTICS_ADROIT_WARNING = (
-    "The AdroitHandRelocateDense-v1, AdroitHandHammerDense-v1, "
-    "AdroitHandDoorDense-v1 environments versions have bug and should be used "
-    "for reproducibility, use gymansium-robotics==1.2.0 if you want to test "
-    "those environments or use version 2 of the environments with "
-    "gymansium-robotics>=1.4.3, see "
-    "https://github.com/Farama-Foundation/Gymnasium-Robotics/pull/220 for more details"
+    "AdroitHandRelocateDense-v1, AdroitHandHammerDense-v1, AdroitHandDoorDense-v1 environment's reward functions were updated in v1.2.1 without an environment version update. Therefore, use gymnasium-robotics==1.2.0 for v1 reproducibility or use v2 in gymnasium-robotics>=1.4.3. See https://github.com/Farama-Foundation/Gymnasium-Robotics/pull/220 for more details"
 )
 
 GYMNASIUM_ROBOTICS_ADROIT_VERSIONS = {
